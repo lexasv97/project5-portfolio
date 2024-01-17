9a913be28394523e3fd44292acc16b1b1384427b
Aleksei <alexey.sv.97@gmail.com>
2024-01-16 19:01:28 -0500

    3rd nd

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -15,10 +15,10 @@ export default function Banner() {
             className="bg-cover bg-center h-screen pt-20 gap-5 flex lg:flex-row flex-col justify-between text-white">
             <div className="lg:pl-32 lg:w-7/12 flex flex-col justify-between lg:py-20">
                 <div className="flex flex-col lg:px-0 px-10">
-                    <span className='text-left font-vazirmatn lg:text-9xl text-5xl'>ALEKSEI</span>
+                    <span className='text-left font-vazirmatn lg:text-9xl text-7xl'>ALEKSEI</span>
                     <div className='text-right lg:pl-64'>
-                        <span className='font-vazirmatn lg:text-9xl text-5xl'>SELETCKII</span>
-                        <p className='lg:text-2xl pt-10 text-left'>Software Engineer - TypeScript | React | JavaScript | ExpressJS | NodeJS | NextJS | MongoDB | PostgreSQL</p>
+                        <span className='font-vazirmatn lg:text-9xl text-7xl'>SELETCKII</span>
+                        <p className='lg:text-2xl pt-10 text-left hidden lg:block'>Software Engineer - TypeScript | React | JavaScript | ExpressJS | NodeJS | NextJS | MongoDB | PostgreSQL</p>
                     </div>
                 </div>
                 <div className="hidden text-4xl w-full lg:flex justify-evenly">
@@ -37,10 +37,26 @@ export default function Banner() {
                 </div>
             </div>
             <div style={{
-            backgroundImage: `url(${img})`,
-        }}
-            className='w-full bg-cover'>
-                {/* <img src={img} className='lg:h-full h-3/4' /> */}
+                backgroundImage: `url(${img})`,
+            }}
+                className='w-full lg:bg-cover object-cover lg:h-full h-5/6 object-right'>
+                <div className='lg:hidden block pr-60 pl-10 text-l pt-16'>
+                    <p className=''>Software Engineer - TypeScript | React | JavaScript | ExpressJS | NodeJS | NextJS | MongoDB | PostgreSQL</p>
+                </div>
+                <div className="lg:hidden text-4xl w-full flex justify-evenly items-end h-2/3">
+                    <Link to="https://github.com/lexasv97" className="hover:scale-150 rounded-full glow p-2" target={'_blank'}>
+                        <AiFillGithub />
+                    </Link>
+                    <Link to="https://www.linkedin.com/in/aleksei-seletckii/" className="hover:scale-150 rounded-full glow p-2" target={'_blank'}>
+                        <FaLinkedinIn />
+                    </Link>
+                    <Link to="https://twitter.com/ASeletckii" className="hover:scale-150 rounded-full glow p-2" target={'_blank'}>
+                        <FiTwitter />
+                    </Link>
+                    <Link to="https://www.facebook.com/lexasv97/" className="hover:scale-150 rounded-full glow p-2" target={'_blank'}>
+                        <BsFacebook />
+                    </Link>
+                </div>
             </div>
         </div>
     )
--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -10,11 +10,11 @@ export default function Contacts() {
     <div style={{
       backgroundImage: `url(${background})`,
     }}
-      className="bg-cover bg-center lg:h-screen w-full grid lg:grid-cols-2 text-white">
+      className="bg-cover bg-center lg:h-screen w-full lg:grid lg:grid-cols-2 text-white">
       <div className="lg:p-20 p-10">
         <div className="flex flex-col justify-center items-center basis-full grow h-full">
-          <h1 className='lg:text-5xl pb-20 font-bold text-3xl'>Let's get in touch</h1>
-          <ul className="flex flex-col justify-between basis-full grow text-2xl">
+          <h1 className='lg:text-5xl lg:pb-20 pb-10 flex justify-center font-bold text-3xl'>Let's get in touch</h1>
+          <ul className="flex flex-col justify-between basis-full grow text-2xl lg:pl-0 pl-10 gap-4">
             <li>Aleksei Seletckii</li>
             <li><Link to="mailto: aleksei.s.webdev@gmail.com">alexey.sv.97@gmail.com</Link></li>
             <li><Link to='tel:+12067954267'>+1 (206) 795 42-67</Link></li>
@@ -22,14 +22,14 @@ export default function Contacts() {
             <li><Link to="https://github.com/lexasv97">github.com/lexasv97</Link></li>
           </ul>
           <div className="flex justify-center box-border w-full text-3xl m-20 font-semibold">
-            <Link className="bg-opacity-40 bg-white px-32 py-4 rounded-lg" to='https://docs.google.com/document/d/19NetbGfj24sNn-GowbYH010mx6TLfXRN0bdDIlIcjzc/edit?usp=sharing' target={'_blank'}>My resume</Link>
+            <Link className="bg-opacity-40 bg-white lg:px-32 px-10 py-4 rounded-lg" to='https://docs.google.com/document/d/19NetbGfj24sNn-GowbYH010mx6TLfXRN0bdDIlIcjzc/edit?usp=sharing' target={'_blank'}>My resume</Link>
           </div>
         </div>
       </div>
       <div style={{
         backgroundImage: `url(${rectangle})`,
       }}
-        className=' bg-cover bg-center h-screen flex justify-end items-end hidden md:flex'>
+        className='bg-cover bg-left h-screen lg:flex justify-end items-end hidden'>
         <img src={img} className="" />
       </div>
     </div>
